1cf8c5e011c84cc6c55c057f723ef6fd0d2ee33e
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-01-27 21:39:39 +0100

    feat: add components do declarations

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/public/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { LoginComponent } from './components/public/login/login.component';
+import { RegisterComponent } from './components/public/register/register.component';
+import { PersonalInfoComponent } from './components/auth/profile/personal-info/personal-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    PersonalInfoComponent
   ],
   imports: [
     BrowserModule,
