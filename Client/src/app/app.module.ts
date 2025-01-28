27d6cd8cdd0611a443320e5c486244fc851dbf76
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-01-28 19:07:01 +0100

    chore: add declarations: WordPolishToEnglish, WordEnglishToPolish, GamesComponents, imports: MaterialModule

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { LoginComponent } from './components/public/login/login.component';
 import { RegisterComponent } from './components/public/register/register.component';
 import { PersonalInfoComponent } from './components/auth/profile/personal-info/personal-info.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WordPolishToEnglishComponent } from './components/games/word-polish-to-english/word-polish-to-english.component';
+import { WordEnglishToPolishComponent } from './components/games/word-english-to-polish/word-english-to-polish.component';
+import { MaterialModule } from './_modules/material/material.module';
+import { GamesComponent } from './components/public/games/games.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +21,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NavbarComponent,
     LoginComponent,
     RegisterComponent,
-    PersonalInfoComponent
+    PersonalInfoComponent,
+    WordPolishToEnglishComponent,
+    WordEnglishToPolishComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
