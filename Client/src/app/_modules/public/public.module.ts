eb80517090ebaa2f2ed0083d5c99f759c48d1ef0
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-01-31 18:09:55 +0100

    feat: add login form

--- Client/src/app/_modules/public/public.module.ts
+++ Client/src/app/_modules/public/public.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { GamesComponent } from 'src/app/components/public/games/games.component';
+import { HomeComponent } from 'src/app/components/public/home/home.component';
+import { LoginComponent } from 'src/app/components/public/login/login.component';
+import { RegisterComponent } from 'src/app/components/public/register/register.component';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GamesComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule, MaterialModule, FormsModule
+  ],
+  exports: [
+    GamesComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ]
 })
 export class PublicModule { }
--- Client/src/app/components/public/login/login.component.ts
+++ Client/src/app/components/public/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { LoginDto } from 'src/app/_models/login';
+import { LoginErrorDto } from 'src/app/_models/login-error';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +11,44 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  hide = signal(true);
+  model: LoginDto = new LoginDto();
+  errors: LoginErrorDto = new LoginErrorDto();
+
+  constructor(private toastr: ToastrService){
+
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  tryLogin(loginForm: NgForm) {
+    this.errors = new LoginErrorDto();
+
+    if (!this.model.email) {
+      this.errors.email = 'E-mail jest wymagany!';
+
+    } else if (!this.isValidEmail(this.model.email)) {
+      this.errors.email = 'Niepoprawny format e-maila!';
+    }
+
+    if (!this.model.password) {
+      this.errors.password = 'Hasło jest wymagane!';
+    }
+
+    if (this.errors.email || this.errors.password) {
+      return;
+    }
+    
+    loginForm.resetForm();
+
+    this.toastr.success('Zalogowano pomyślnie!');
+  }
+
+  private isValidEmail(email: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
 }
