364cb525a9154a0a109ffa5128d0f18df032db3b
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-02-02 15:53:39 +0100

    feat: add register form

--- Client/src/app/components/public/register/register.component.ts
+++ Client/src/app/components/public/register/register.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { LoginErrorDto } from 'src/app/_models/login-error';
+import { RegisterDto } from 'src/app/_models/register';
+import { RegisterErrorDto } from 'src/app/_models/register-error';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  hidePassword = signal(true);
+  hideRepeatPassword = signal(true);
+  model: RegisterDto = new RegisterDto();
+  errors: RegisterErrorDto = new RegisterErrorDto();
 
+  constructor(private toastr: ToastrService) {}
+
+  clickPassword(event: MouseEvent) {
+    this.hidePassword.set(!this.hidePassword());
+    event.stopPropagation();
+  }
+
+  clickRepeatPassword(event: MouseEvent) {
+    this.hideRepeatPassword.set(!this.hideRepeatPassword());
+    event.stopPropagation();
+  }
+
+  tryRegister(registerForm: NgForm) {
+    this.errors = new RegisterErrorDto();
+
+    if (!this.model.email) {
+      this.errors.email = 'E-mail jest wymagany!';
+    } else if (!this.isValidEmail(this.model.email)) {
+      this.errors.email = 'Niepoprawny format e-maila!';
+    }
+
+    if (!this.model.password) {
+      this.errors.password = 'Hasło jest wymagane!';
+    }
+
+    if (!this.model.repeatPassword) {
+      this.errors.repeatPassword = 'Hasło jest wymagane!';
+    } else if (this.model.password != this.model.repeatPassword) {
+      this.errors.repeatPassword = 'Podane hasła są różne!';
+    }
+
+    if (
+      this.errors.email ||
+      this.errors.password ||
+      this.errors.repeatPassword
+    ) {
+      return;
+    }
+
+    registerForm.resetForm();
+
+    // TO DO - Implement API authentication and handle response
+
+    this.toastr.success('Zarejestrowano pomyślnie!');
+  }
+
+  private isValidEmail(email: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
 }
