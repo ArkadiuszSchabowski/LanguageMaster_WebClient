9a5a99f61b9f97f7f3a1a2d2c47018bf2f36b9cc
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-02-04 20:02:28 +0100

    chore: add material and browser animations modules to imports

--- Client/src/app/components/auth/level/a1/lesson1-a1/lesson1-a1.component.spec.ts
+++ Client/src/app/components/auth/level/a1/lesson1-a1/lesson1-a1.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Lesson1A1Component } from './lesson1-a1.component';
+import { MaterialModule } from 'src/app/_modules/material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('Lesson1A1Component', () => {
   let component: Lesson1A1Component;
@@ -8,7 +10,8 @@ describe('Lesson1A1Component', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [Lesson1A1Component]
+      declarations: [Lesson1A1Component],
+      imports: [BrowserAnimationsModule, MaterialModule]
     });
     fixture = TestBed.createComponent(Lesson1A1Component);
     component = fixture.componentInstance;
